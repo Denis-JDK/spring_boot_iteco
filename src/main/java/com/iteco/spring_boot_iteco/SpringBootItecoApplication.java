e3a902b3fdae147f287eee60c4979ccdfe299742
Shapoval Denis <speleo04@yandex.ru>
2023-11-18 13:20:50 +0300

    Initial commit

---
+++ src/main/java/com/iteco/spring_boot_iteco/SpringBootItecoApplication.java
@@ -0,0 +1,13 @@
+package com.iteco.spring_boot_iteco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootItecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootItecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iteco/spring_boot_iteco/SpringBootItecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iteco.spring_boot_iteco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootItecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
