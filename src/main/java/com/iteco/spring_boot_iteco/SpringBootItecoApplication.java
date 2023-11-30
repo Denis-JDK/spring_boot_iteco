e8006b47b6e3563012ecfe4c84fc67bc537403ac
Shapoval Denis <speleo04@yandex.ru>
2023-11-30 18:54:22 +0300

    Spring Boot

--- src/main/java/com/iteco/spring_boot_iteco/SpringBootItecoApplication.java
+++ src/main/java/com/iteco/spring_boot_iteco/SpringBootItecoApplication.java
@@ -4,19 +4,21 @@ package com.iteco.spring_boot_iteco;
 import com.iteco.spring_boot_iteco.home_worke.ExternalInfo;
 import com.iteco.spring_boot_iteco.home_worke.ExternalService;
 import com.iteco.spring_boot_iteco.home_worke.ExternalServiceImpl;
+import com.iteco.spring_boot_iteco.model.AccountInfo;
 import com.iteco.spring_boot_iteco.model.BankBook;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan("com.*")
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yaml")
 public class SpringBootItecoApplication {
 	public static void main(String[] args) {
 
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringBootItecoApplication.class);
 
 		 BankBook bank = context.getBean(BankBook.class);
+		AccountInfo accountInfo = context.getBean(AccountInfo.class);
 		System.out.println(bank);
 		 bank.setId(120);
 		System.out.println(bank);
--- src/main/java/com/iteco/spring_boot_iteco/model/AccountInfo.java
+++ src/main/java/com/iteco/spring_boot_iteco/model/AccountInfo.java
@@ -1,4 +1,7 @@
 package com.iteco.spring_boot_iteco.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AccountInfo {
 }
